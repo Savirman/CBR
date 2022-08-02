f43eb8bb87561aa6664b86d4fc2b24d6bd06422c
Dmitry <savirm@yandex.ru>
2022-08-02 21:53:50 +0900

    Add templates and index.html

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return "Информация по курсам валют"
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run()
