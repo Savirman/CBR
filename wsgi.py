24b626db599b3b10bb2eae46f8b32d0ccc0cf73c
Dmitry <savirm@yandex.ru>
2022-08-04 15:45:53 +0900

    Add styles.css, forms in index.html

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import main
 import db
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", date = main.valkurs['Date'])
+    return render_template("index.html", date = main.valkurs['Date'], current_date = main.current_date)
 
 
 if __name__ == '__main__':
